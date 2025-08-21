37f0b17d96d161cb76f6836290e16964282201e0
Vigamy <andreottiwill@gmail.com>
2025-08-21 02:50:29 -0300

    testing git

--- app/src/main/java/com/will/busnotification/MainActivity.kt
+++ app/src/main/java/com/will/busnotification/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.will.busnotification.data.api.GoogleApiInstance
-import com.will.busnotification.data.api.GoogleApiInterface
 import com.will.busnotification.ui.BusListScreen
 import com.will.busnotification.ui.theme.BusNotificationTheme
 
